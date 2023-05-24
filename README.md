e611c2f4c208dbb434f5398e314b3c581e171617
Emran Ramezan <Emran@opulence.design>
2023-05-24 16:37:38 +0100

    working tree-sitter. wip, refactoring, bug check

--- README.md
+++ README.md
@@ -1,23 +1,43 @@
 # Tree-Sitter-Blade
 
-This project aims to write the tree-sitter grammar for Laravel Blade since there is none available at the moment. I am hoping to get some help from kind people of the open source community to accomplish this and fine tuning the nitty gritties as this is my very first time writing any lang grammar let alone a tree-sitter one! 🙏
+This project aims to write the tree-sitter grammar for Laravel Blade
+since there is none available at the moment. I am hoping to get some
+help from the kind people of the open source community to accomplish
+this and fine tuning the nitty gritties as this is my very first time
+writing any language grammar let alone a tree-sitter one! 🙏
 
-I am using Nova editor by Panic 🤘, but I believe the grammars should be universal for all editors including **NeoVim** or **Zed** (RIP _Atom_ 🪦). However I am not sure how it can be ported to those editors, but I assume it should be a fairly easy task once the grammar is done.
+I am using Nova editor by Panic 🤘, but I believe the grammars should
+be universal for all editors including **NeoVim** or **Zed** (RIP
+_Atom_ 🪦). However I am not sure how it can be ported to those
+editors, but I assume it should be a fairly easy task once the grammar
+is done.
 
 ## Quick note
 
-I have left some folders intentionally empty, for example as I understand some editors use `queries/` folder as it is but some like Nova have their own `Query/` folder with specific `.scm` structuring. I have left it empty for now.
+I have left some folders intentionally empty for time being. For
+example as I understand some editors may use `queries/` folder as it
+is but some like Nova have their own `Query/` folder with specific
+`.scm` structuring. But feel free to pull request and write `.scm` for
+that folder.
 
 ## For Nova Contributors
 
-You would need to compile the parser to `.dylib` and then copy it in the `Syntax/` folder of the extension. [blade extension for Nova](https://github.com/EmranMR/nova-blade)
+Headover to this repo:
 
-You can download the build script here: [**Parser Build Script**](https://docs.nova.app/syntax-reference/build_script.zip)
+-   [blade extension for Nova](https://github.com/EmranMR/nova-blade)
 
-Please refer to the README file in that repo for more information about contributing and using the build script.
+You need to compile the parser to `.dylib` and then copy it in the
+`Syntax/` folder of the extension.
+
+You can download the build script here:
+[**Parser Build Script**](https://docs.nova.app/syntax-reference/build_script.zip)
+
+Please refer to the README file in that repo for more information
+about contributing and using the build script.
 
 ## Todos
 
 -   [ ] Write the grammar
 -   [ ] Write the tests
+-   [ ] Support Livewire 🪼
 -   [ ] Write queries?
--- grammar.js
+++ grammar.js
@@ -1,9 +1,199 @@
 module.exports = grammar({
     name: 'blade',
+    extras: ($) => [$.comment, /\s+/],
 
     rules: {
-        program: ($) => repeat($._definition),
-        _definition: ($) => choice($.body),
-        body: ($) => /[a-z]+/,
+        blade: ($) => repeat($._definition),
+        _definition: ($) =>
+            choice(
+                $.text,
+                $.extends,
+                $.yield,
+                $.keyword,
+                $.fragment,
+                $.component,
+                $.subview,
+                $.stack,
+                $.switch_statements,
+                $.loop,
+                $.php_statement,
+                $.html_statement
+            ),
+
+        comment: ($) => seq('{{--', optional($.text), '--}}'),
+        extends: ($) => seq('@extends', $.directive_parameter),
+        yield: ($) => seq('@yield', $.directive_parameter),
+        subview: ($) =>
+            choice(
+                $.include,
+                $.includeIf,
+                $.includeWhen,
+                $.includeUnless,
+                $.includeFirst
+            ),
+
+        // !subview
+        include: ($) => seq('@include', $.directive_parameter),
+        includeIf: ($) => seq('@includeIf', $.directive_parameter),
+        includeWhen: ($) =>
+            seq('@includeWhen', $.directive_parameter),
+        includeUnless: ($) =>
+            seq('@includeUnless', $.directive_parameter),
+        includeFirst: ($) =>
+            seq('@includeFirst', $.directive_parameter),
+
+        // ! once
+        stack: ($) =>
+            seq(
+                /@(push|pushOnce|pushIf|prepend)/,
+                $.directive_parameter,
+                repeat($._definition),
+                /@(endpush|endPushOnce|endPushIf|endprepend)/
+            ),
+
+        each: ($) => seq('@each', $.directive_parameter),
+        // ! php Blocks
+        php_statement: ($) => choice($._escaped, $._unescaped, $._raw),
+        _escaped: ($) => seq('{{', optional($.text), '}}'),
+        _unescaped: ($) => seq('{!!', optional($.text), '!!}'),
+        _raw: ($) =>
+            choice($.multi_line_raw, $.inline_raw, $.classic_raw),
+        multi_line_raw: ($) =>
+            seq('@php', optional($.text), '@endphp'),
+        inline_raw: ($) => seq('@php', $.directive_parameter),
+        classic_raw: ($) => seq('<?php', optional($.text), '?>'),
+
+        // ! HTML blocks
+        html_statement: ($) => choice($.conditional, $.section),
+
+        // ! Javascript Blocks
+        // TODO:Stuff that need JS injection
+
+        // !Attributes
+        // TODO: @class @style @checked @selected @disabled @readonly @required
+
+
+
+        // !Conditionals
+        conditional: ($) =>
+            choice(
+                $.if_statement,
+                $.directive
+            ),
+        directive: $ => 
+            seq(
+                /@(auth|guest|env|production|unless|isset|empty|hasSection|sectionMissing|verbatim|error|disk|unlessdisk|once)/,
+                optional($.directive_parameter),
+                repeat($._definition),
+                /@(endauth|endguest|endenv|endproduction|endunless|endisset|endempty|verbatim|enderror|enddisk|endonce)/
+            ), 
+        if_statement: ($) =>
+            seq(
+                "@if",
+                $.directive_parameter,
+                repeat(choice(
+                    $._definition,
+                    $.conditional_keyword,
+                )),
+                "@endif",
+            ),
+        conditional_keyword: $=> 
+            seq(
+                /@(else|elseif|elsedisk)/,
+                optional($.directive_parameter)
+            ), 
+
+        // !switch //
+        // REVIEW: injection for param as PHP for case as HTML
+        switch_statements: ($) =>
+            seq(
+                '@switch',
+                $.directive_parameter,
+                repeat1($.case),
+                optional(seq('@default', $._definition)),
+                '@endswitch'
+            ),
+        case: ($) =>
+            seq(
+                '@case',
+                $.directive_parameter,
+                $._definition,
+                '@break'
+            ),
+
+        // ! Loops
+        loop: ($) =>
+            seq(
+                /@(for|foreach|forelse|while)/,
+                $.directive_parameter,
+                repeat1(choice($._definition, $.loop_operator)),
+                optional(seq('@empty', repeat($._definition))),
+                /@(endfor|endforeach|endforelse|endwhile)/
+            ),
+        // REVIEW: refactor name? or separate $.continue,$.break
+        // FIXME: only works in the loop body NOT if_stemenets etc
+        // maybe make universal? or put in def?
+        loop_operator: ($) =>
+            seq(/@(continue|break)/, optional($.directive_parameter)),
+
+        // ! Javascript Statement //
+        // ------------------------
+        // REVIEW: This needs investigation with experienced users
+        // create an issue when live
+        // This is for Verbatim, @@blade_directives and @{{}} JS escapes
+        // @verbatim currently defined in conditional rule
+        //
+        // js_statement: ($) => x ,
+
+        // !section
+        section: ($) =>
+            seq(
+                '@section',
+                $.directive_parameter,
+                optional(repeat($._definition)),
+                '@endsection'
+            ),
+        // !fragment
+        // TODO: double check
+        fragment: ($) =>
+            seq(
+                '@fragment',
+                $.directive_parameter,
+                optional(repeat($._definition)),
+                '@endfragment'
+            ),
+
+        // !component
+        // TODO: make sure the attributes work
+        component: ($) =>
+            choice($._self_closing_component, $._parent_component),
+
+        _self_closing_component: ($) => /<x-[a-zA-Z\-\.:\s]+\/>/,
+        _parent_component: ($) =>
+            seq(
+                /<x-[a-zA-Z\-\.\s]+>/,
+                optional(repeat($._definition)),
+                /<\/x-[a-zA-Z\-\.]+>/
+            ),
+        // !keywords
+        keyword: ($) =>
+            choice($.props, $.csrf, $.method, $.inject),
+        props: ($) => seq('@props(', $.text, ')'),
+        csrf: ($) => '@csrf',
+        method: ($) => seq('@method', $.directive_parameter),
+        inject: ($) => seq('@inject', $.directive_parameter),
+
+        //! Misc
+        // text: ($) => /[^<>&\s@]([^<>&@{!}]*[^<>&\s@{!}])?/,
+        text: ($) => /[^\s@]([^@{!}()\-]*[^\s@{!}()\-])?/,
+        _open_parenthesis: ($) => /\([\'\"]?/,
+        _close_parenthesis: ($) => /[\'\"]?\)/,
+        // REVIEW: optional or not optional statement?
+        directive_parameter: ($) =>
+            seq(
+                $._open_parenthesis,
+                optional($.text),
+                $._close_parenthesis
+            ),
     },
 })
