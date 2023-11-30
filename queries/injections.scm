3f41745265e5389ffc9e83d61e4a2b1d6cbb0ee7
Emran Ramezan <Emran@opulence.design>
2023-11-30 12:03:14 +0000

    fixes #38, closes #29

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,13 +1,17 @@
-((php) @injection.content
+((text) @injection.content
+    (#not-has-ancestor? @injection.content "envoy")
     (#set! injection.combined)
     (#set! injection.language php))
 
+((text) @injection.content
+    (#has-ancestor? @injection.content "envoy")
+    (#set! injection.combined)
+    (#set! injection.language shell))
 
 
-;((php_only) @injection.content
-;    (#set! injection.combined)
-;    (#set! injection.language php_only))
+((php_only) @injection.content
+    (#set! injection.combined)
+    (#set! injection.language php_only))
+((parameter) @injection.content
+    (#set! injection.language php_only))
 
-; directive parameters
-;((parameter) @injection.content
-;    (#set! injection.language php_only))
