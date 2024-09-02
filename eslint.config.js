5eae5e1677191bb42506bf160906e34612a438b0
Emran Ramezan <Emran@opulence.design>
2024-09-02 14:09:40 +0100

    eslint flat config migration

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,28 @@
-module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-  },
-  extends: 'google',
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'max-len': [
-      'error',
-      {
-        code: 120,
-        ignoreComments: true,
-        ignoreUrls: true,
-        ignoreStrings: true,
-      },
-    ],
-  },
-};
+import globals from "globals"
+import google from "eslint-config-google"
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.commonjs,
+        ...globals.es2021,
+      }
+    },
+    ...google,
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'max-len': [
+        'error',
+        {
+          code: 120,
+          ignoreComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+        },
+      ],
+    },
+  }
+]
