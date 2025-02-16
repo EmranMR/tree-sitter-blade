300b0739c1881e0a46ea9e06f1b5d6a2d85763b0
Emran Ramezan <Emran@opulence.design>
2025-02-16 19:43:25 +0000

    tree-sitter-html submodule set up

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -2,3 +2,17 @@
 (directive_start) @tag
 (directive_end) @tag
 (comment) @comment
+
+(tag_name) @tag
+(erroneous_end_tag_name) @tag.error
+(doctype) @constant
+(attribute_name) @attribute
+(attribute_value) @string
+(comment) @comment
+
+[
+  "<"
+  ">"
+  "</"
+  "/>"
+] @punctuation.bracket
