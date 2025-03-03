837f6b4d7ac4bb7fc0eb967bc8707f9186692b05
Emran Ramezan <Emran@opulence.design>
2025-03-03 11:11:10 +0000

    adding scanner to rust build script

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -2,7 +2,10 @@ fn main() {
     let src_dir = std::path::Path::new("src");
 
     let mut c_config = cc::Build::new();
-    c_config.std("c11").include(src_dir).flag_if_supported("-Wno-unused-value");
+    c_config
+        .std("c11")
+        .include(src_dir)
+        .flag_if_supported("-Wno-unused-value");
 
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
@@ -11,9 +14,9 @@ fn main() {
     c_config.file(&parser_path);
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
-    // let scanner_path = src_dir.join("scanner.c");
-    // c_config.file(&scanner_path);
-    // println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
+    let scanner_path = src_dir.join("scanner.c");
+    c_config.file(&scanner_path);
+    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
 
     c_config.compile("tree-sitter-blade");
 }
